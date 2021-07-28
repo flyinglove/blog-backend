6f6a642ab8dc5ea35239f8252ed2f406bec851e4
flyinglove <446419676@qq.com>
2021-07-28 22:15:37 +0800

    feat: commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c85cf605d44c611c7cbd78c195847b2d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7c3e44bb-89ca-49d5-80b5-1efa7e3e18ee'
+};
